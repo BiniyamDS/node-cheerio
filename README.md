208538fdf737c4600c028d346c46f7a7b26d9e22
BiniyamDS <biniyam.yr@gmail.com>
2024-04-08 21:42:08 +0300

    formatted response text

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ using `cheerio`
 ## Things to do
 - [x] Fetch data from url
 - [x] Apply css selector and print text
-- [ ] Format text
+- [x] Format text
 - [ ] Get user input from commandline and make it dynamic
--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 const url = "http://books.toscrape.com/";
-const selector = 'product_price'
+const selector = "product_price";
 
 async function getBody() {
   try {
     const response = await axios.get(url);
     const $ = cheerio.load(response.data);
-    console.log($(`.${selector}`).text())
+    const resultText = $(`.${selector}`).text();
+
+    console.log(resultText.replace(/\s+/g, " "));
   } catch (err) {
     console.log(`Error fetching data: ${err}`);
   }
